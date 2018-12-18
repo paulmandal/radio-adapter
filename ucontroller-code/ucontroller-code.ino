c9d06cab480d24c4463fd46ffb13e8490b044789
Paul Mandal <paul.mandal@gmail.com>
2018-12-17 18:47:35 -0700

    Add the ability to disable debugging output using compiler directives

--- ucontroller-code/ucontroller-code.ino
+++ ucontroller-code/ucontroller-code.ino
@@ -2,6 +2,9 @@
  * Transforms GPS NMEA messages from the variable-length format used by Garmin (and probably others) into
  * the fixed-lenght format used by the Yaesu VX-8DR
  */
+// Uncomment the line below to enable debugging output to the UC-DBG port
+#define DEBUG_PORT_OUTPUT_ENABLED
+
 #include <NeoSWSerial.h>
 #include <NeoHWSerial.h>
 
@@ -21,16 +24,21 @@ boolean xferMessages = true;
 extern const TransformMapping transformMap[];
 
 NeoSWSerial radioSerial(9, 8);
+
+#ifdef DEBUG_PORT_OUTPUT_ENABLED
 NeoSWSerial debugSerial(7, 6);
+#endif
 
 void setup() {
   NeoSerial.attachInterrupt(handleGpsInput);
   NeoSerial.begin(4800);
   radioSerial.begin(9600);
 
+#ifdef DEBUG_PORT_OUTPUT_ENABLED
   debugSerial.attachInterrupt(handleDebugInput);
   debugSerial.begin(9600);
   debugSerial.println("Debug serial output - application started.");
+#endif
 }
 
 /**
@@ -79,15 +87,20 @@ void handleSerialInput(char c, volatile MessageBuffer &messageBuffer) {
  * Handle a completed message -- determine the message type and apply the appropriate transformation
  */
 void handleMessage(char *message) {
+#ifdef DEBUG_PORT_OUTPUT_ENABLED
   debugSerial.print("GPS->uC:   ");
   debugSerial.print(message);
+#endif
 
   // Check for debugging messages
   if(strncmp(message, "$XFER", 5) == 0) {
     // handle debug message transfer toggle command
     xferMessages = !xferMessages;
+
+#ifdef DEBUG_PORT_OUTPUT_ENABLED
     debugSerial.print("xferMessages: ");
     debugSerial.println(xferMessages);
+#endif
   }
 
   if(!xferMessages) {
@@ -117,8 +130,10 @@ void sendToRadio(char *message) {
   radioSerial.listen();
   radioSerial.print(message);
 
+#ifdef DEBUG_PORT_OUTPUT_ENABLED
   debugSerial.listen();
   debugSerial.print("uC->radio: ");
   debugSerial.print(message);
+#endif
 }
 
